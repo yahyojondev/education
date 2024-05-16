20f41592d43416a4a2b53f437d047e93e2c8e893
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-16 16:04:29 +0500

    work

--- src/components/universityPagenine/UniversityPagenine.jsx
+++ src/components/universityPagenine/UniversityPagenine.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Mousewheel, Keyboard } from "swiper/modules";
-import { dreamproduct } from "../../static/router";
+import { universityproduct } from "../../static/router";
 
 const Pagenine = () => {
-  const pagenineItem = dreamproduct?.map((el) => (
+  const pagenineItem = universityproduct?.map((el) => (
     <SwiperSlide key={el.id}>
       <div className="pagenine__card">
         <img src={el.img} alt={el.title} />
         <div className="pagenine__card__content">
-          <p className="university__pagenine__title">{el.title}</p>
+          <p>{el.title}</p>
           <span>${el.price}</span>
         </div>
       </div>
@@ -19,9 +19,9 @@ const Pagenine = () => {
     <div className="pagenine">
       <div className="container">
         <Swiper
-          slidesPerView={4}
+          slidesPerView={1}
           spaceBetween={30}
-          //   centeredSlides={true}
+          centeredSlides={true}
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -33,6 +33,57 @@ const Pagenine = () => {
           modules={[Autoplay, Navigation, Mousewheel, Keyboard]}
           loop={true}
           className="pagenine__cards"
+        >
+          {pagenineItem}
+        </Swiper>
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={30}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          cssMode={true}
+          navigation={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Autoplay, Navigation, Mousewheel, Keyboard]}
+          loop={true}
+          className="pagenine__cards pegenine__cards__two"
+        >
+          {pagenineItem}
+        </Swiper>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          cssMode={true}
+          navigation={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Autoplay, Navigation, Mousewheel, Keyboard]}
+          loop={true}
+          className="pagenine__cards pegenine__cards__three"
+        >
+          {pagenineItem}
+        </Swiper>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          cssMode={true}
+          navigation={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Autoplay, Navigation, Mousewheel, Keyboard]}
+          loop={true}
+          className="pagenine__cards pegenine__cards__four"
         >
           {pagenineItem}
         </Swiper>
